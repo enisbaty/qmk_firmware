26fa3c862a8605d99aae7ea1ef2617087c6ef5ee
Enis Baty <32305538+enisbaty@users.noreply.github.com>
2021-06-28 23:23:20 +0100

    Change layout to match standard ortho on RHS

--- keyboards/preonic/keymaps/enisbaty/keymap.c
+++ keyboards/preonic/keymaps/enisbaty/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |XXXXXX|   1  |   2  |   3  |   4  |   5  |   6  |   7  |  8   |   9  |  0   |XXXXXX|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |   -  |  =   |  [   |  ]   | PRSC | PGUP | HOME | INS  |XXXXXX|XXXXXX|  |   |
+ * |      |   -  |  =   |  [   |  ]   | PRSC | PGUP | HOME | INS  |XXXXXX|  |   |XXXXXX|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol+ | Play |      |
  * `-----------------------------------------------------------------------------------'
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , XXXXXXX,
-    _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PSCR, KC_PGUP, KC_HOME, KC_INS , XXXXXXX, XXXXXXX, KC_PIPE,
+    _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PSCR, KC_PGUP, KC_HOME, KC_INS , XXXXXXX, KC_PIPE, XXXXXXX,
     _______, _______, _______, _______, XXXXXXX, _______, _______, XXXXXXX, KC_MNXT, KC_VOLU, KC_MPLY, _______
 ),
 
@@ -70,9 +70,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   '  |  /   |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl | GUI  | Alt  | F13  |Raise |    Space    |Lower | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
@@ -80,8 +80,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_preonic_grid( \
     KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, KC_SLSH,
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
     KC_LCTL, KC_LGUI, KC_LALT, KC_F13,  RAISE,   KC_SPC,  KC_SPC,  LOWER,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
@@ -92,9 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |XXXXXX|XXXXXX|XXXXXX|XXXXXX|XXXXXX|XXXXXX|XXXXXX|XXXXXX|XXXXXX|XXXXXX|XXXXXX|XXXXXX|
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |CAPSLK|  !   |  @   |  #   |  $   |  %   |  ^   |   &  |   *  |  (   |   )  | Ent  |
+ * |CAPSLK|  !   |  @   |  #   |  $   |  %   |  ^   |   &  |   *  |  (   |   )  |XXXXXX|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  _   |  +   |  {   |  }   |PAUSE | PGDN | END  |XXXXXX|XXXXXX|XXXXXX|  \   |
+ * |      |  _   |  +   |  {   |  }   |PAUSE | PGDN | END  |XXXXXX|XXXXXX|  \   |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Prev | Vol- | Stop |      |
  * `-----------------------------------------------------------------------------------'
@@ -102,8 +102,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_preonic_grid( \
     KC_GRV , RGB_TOG, RGB_VAD, RLI_TOG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    KC_CAPS, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_ENT ,
-    _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PAUS, KC_PGDN, KC_END , XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS,
+    KC_CAPS, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
+    _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PAUS, KC_PGDN, KC_END , XXXXXXX, XXXXXXX, KC_BSLS, KC_ENT,
     _______, _______, _______, _______, XXXXXXX, _______, _______, XXXXXXX, KC_MPRV, KC_VOLD, KC_MSTP, _______
 )
 
