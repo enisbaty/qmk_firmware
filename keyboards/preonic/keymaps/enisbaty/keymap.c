da869698f1cf9d69b0c812b0ed28ad158f077630
Enis Baty <32305538+enisbaty@users.noreply.github.com>
2021-07-21 15:16:40 +0100

    Add enter to all layers preonic

--- keyboards/preonic/keymaps/enisbaty/keymap.c
+++ keyboards/preonic/keymaps/enisbaty/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |XXXXXX|   1  |   2  |   3  |   4  |   5  |   6  |   7  |  8   |   9  |  0   |XXXXXX|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |   -  |  =   |  [   |  ]   | PRSC | PGUP | HOME | INS  |XXXXXX|  |   |XXXXXX|
+ * |      |   -  |  =   |  [   |  ]   | PRSC | PGUP | HOME | INS  |XXXXXX|  |   |KC_ENT|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol+ | Play |      |
  * `-----------------------------------------------------------------------------------'
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , XXXXXXX,
-    _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PSCR, KC_PGUP, KC_HOME, KC_INS , XXXXXXX, KC_PIPE, XXXXXXX,
+    _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PSCR, KC_PGUP, KC_HOME, KC_INS , XXXXXXX, KC_PIPE, KC_ENT ,
     _______, _______, _______, _______, XXXXXXX, _______, _______, XXXXXXX, KC_MNXT, KC_VOLU, KC_MPLY, _______
 ),
 
